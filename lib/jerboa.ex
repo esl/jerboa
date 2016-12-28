ec24ec6b78014efccdebe0a904101e9dfc7e0f61
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2016-12-28 09:41:12 +0100

    Create `Jerboa.Format` part of a library

--- lib/jerboa.ex
+++ lib/jerboa.ex
@@ -2,7 +2,9 @@ defmodule Jerboa do
   @moduledoc """
   STUN/TURN encoder, decoder and client library
 
-  For the client utilities, see `Jerboa.Client`
+  For the client utilities, see `Jerboa.Client`.
+
+  For encoding and decoding of STUN format, head to `Jerboa.Format`.
   """
 
 end
