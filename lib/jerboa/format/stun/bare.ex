5a82a236fa7834abb4042a8b61e73d63d1a61123
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2016-12-29 10:45:41 +0100

    Add decoding of STUN message attributes

--- lib/jerboa/format/stun/bare.ex
+++ lib/jerboa/format/stun/bare.ex
@@ -7,7 +7,7 @@ defmodule Jerboa.Format.STUN.Bare do
 
   @stun_magic STUN.magic
 
-  @type bare_attr :: {type :: binary, value :: binary}
+  @type bare_attr :: {type :: non_neg_integer, value :: binary}
 
   @type t :: %__MODULE__{
       t_id: non_neg_integer,
@@ -25,11 +25,14 @@ defmodule Jerboa.Format.STUN.Bare do
                   t_id <- extract_transaction_id(header),
                   type <- extract_stun_type(header),
                  class <- decode_message_class(type),
-                method <- extract_message_method(type) do
+                method <- extract_message_method(type),
+     {:ok, body, rest} <- validate_body_length(packet),
+          {:ok, attrs} <- extract_attributes(body) do
       struct = %__MODULE__{
         t_id: t_id,
         class: class,
         method: method,
+        attrs: Enum.reverse(attrs),
         raw: packet
       }
       {:ok, struct}
@@ -66,5 +69,57 @@ defmodule Jerboa.Format.STUN.Bare do
   defp extract_message_method(<<m2::5, _::1, m1::3, _::1, m0::4>>) do
     <<method::12>> = <<m2::5, m1::3, m0::4>>
     method
+  end
+
+  defp validate_body_length(<<_::16, length::16, _::128,
+                              body::size(length)-binary, rest::bitstring>>) do
+    {:ok, body, rest}
+  end
+  defp validate_body_length(_), do: {:error, "Invalid message body length"}
+
+  defp extract_attributes(body, acc \\ [])
+  defp extract_attributes(<<>>, acc), do: {:ok, acc}
+  defp extract_attributes(body, acc) do
+    with {:ok, type, length, val_and_rest} <- extract_attr_type_and_length(body),
+                {:ok, value, pad_and_rest} <- extract_attr_value(length, val_and_rest),
+                               {:ok, rest} <- trim_padding(pad_and_rest, length) do
+      extract_attributes(rest, [{type, value} | acc])
+    else
+      {:error, _reason} = error ->
+        error
+    end
+  end
+
+  defp extract_attr_type_and_length(<<type::16, length::16, rest::binary>>) do
+    {:ok, type, length, rest}
+  end
+  defp extract_attr_type_and_length(_) do
+    {:error, "Not enough bytes for attribute"}
+  end
+
+  defp extract_attr_value(length, body) do
+    case body do
+      <<value::size(length)-binary, rest::binary>> ->
+        {:ok, value, rest}
+      _ ->
+        {:error, "Not enough bytes for attribute value"}
+    end
+  end
+
+  defp trim_padding(body, length) do
+    pad_len = calculate_padding(length)
+    case body do
+      <<_::size(pad_len)-binary, rest::binary>> ->
+        {:ok, rest}
+      _ ->
+        {:error, "No attribute padding"}
+    end
+  end
+
+  defp calculate_padding(length) do
+    case rem(length, 4) do
+      0 -> 0
+      n -> 4 - n
+    end
   end
 end
--- test/jerboa/format/stun/bare_test.exs
+++ test/jerboa/format/stun/bare_test.exs
@@ -1,5 +1,5 @@
 defmodule Jerboa.Format.STUN.BareTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   use Quixir
 
   alias Jerboa.Format.STUN.Bare
@@ -34,13 +34,13 @@ defmodule Jerboa.Format.STUN.BareTest do
     end
 
     test "extracts method and decodes class from the packet" do
-      ptest method: int(min: 0), class: int(min: 0, max: 3), t_id: int(min: 0),
-            length: int(min: 0), body: int(min: 0) do
+      ptest method: int(min: 0), class: int(min: 0, max: 3),
+            t_id: int(min: 0) do
         <<c1::1, c0::1>> = <<class::2>>
         <<m2::5, m1::3, m0::4>> = <<method::12>>
         magic = Jerboa.Format.STUN.magic
-        packet = <<0::2, m2::5, c1::1, m1::3, c0::1, m0::4, length::16,
-                   magic::32, t_id::96, body::size(length)-unit(8)>>
+        packet = <<0::2, m2::5, c1::1, m1::3, c0::1, m0::4, 0::16,
+                   magic::32, t_id::96>>
 
         {:ok, bare} = Bare.decode packet
         decoded_class = Jerboa.Format.STUN.class_from_integer(class)
@@ -49,5 +49,88 @@ defmodule Jerboa.Format.STUN.BareTest do
         assert decoded_class == bare.class
       end
     end
+
+    test "fails given packet with too short message body" do
+      ptest type: int(min: 0), t_id: int(min: 0), length: int(min: 1000),
+            body: int(min: 0), body_length: int(min: 0, max: ^length - 1) do
+        magic = Jerboa.Format.STUN.magic
+        packet = <<0::2, type::14, length::16, magic::32, t_id::96,
+                   body::unit(8)-size(body_length)>>
+
+        assert {:error, _} = Bare.decode packet
+      end
+    end
+
+    test "extracts attributes from message body" do
+      ptest type: int(min: 0), t_id: int(min: 0),
+            attrs: list(of: tuple(like: {int(min: 0), string()})) do
+        bin_attrs = encode_attributes(attrs)
+        packet = create_packet(type, t_id, bin_attrs)
+
+        {:ok, bare} = Bare.decode packet
+
+        assert attrs == bare.attrs
+      end
+    end
+
+    test "fails if body does not have enough bytes for attribute" do
+      ptest type: int(min: 0), t_id: int(min: 0), extra: int(min: 1, max: 3),
+            attrs: list(of: tuple(like: {int(min: 0), string()})) do
+        bin_attrs = encode_attributes(attrs)
+        bin_attrs = <<bin_attrs::binary, 1::size(extra)-unit(8)>>
+        packet = create_packet(type, t_id, bin_attrs)
+
+        assert {:error, _} = Bare.decode packet
+      end
+    end
+
+    test "fails if there are not enough bytes for attribute value" do
+      ptest type: int(min: 0), t_id: int(min: 0), attr_length: int(min: 0),
+            attr_type: int(min: 0), attr_value: int() do
+        bin_attr = <<attr_type::16, (attr_length + 1)::16,
+                     attr_value::size(attr_length)-unit(8)>>
+        packet = create_packet(type, t_id, bin_attr)
+
+        assert {:error, _} = Bare.decode packet
+      end
+    end
+
+    test "fails if there is no attribute padding" do
+      ptest type: int(min: 0), t_id: int(min: 0), attr_type: int(min: 0),
+            attr_value: int(), attr_length: int(min: 0) do
+        attr_length =
+          if rem(attr_length, 4) == 0, do: attr_length + 1, else: attr_length
+        bin_attr = <<attr_type::16, attr_length::16,
+                     attr_value::size(attr_length)-unit(8)>>
+        packet = create_packet(type, t_id, bin_attr)
+
+        assert {:error, _} = Bare.decode packet
+      end
+    end
+  end
+
+  ## Test helpers
+
+  defp calculate_padding(length) do
+    case rem(length, 4) do
+      0 -> 0
+      n -> 4 - n
+    end
+  end
+
+  defp create_packet(type, t_id, body) do
+    length = byte_size(body)
+    magic = Jerboa.Format.STUN.magic
+    header = <<0::2, type::14, length::16, magic::32, t_id::96>>
+    <<header::binary, body::binary>>
+  end
+
+  defp encode_attributes(attrs) do
+    Enum.map(attrs, fn {type, value} ->
+      length = byte_size(value)
+      pad_len = calculate_padding(length)
+      <<type::16, length::16, value::binary, 0::size(pad_len)-unit(8)>>
+    end)
+    |> Enum.join()
   end
 end
