d5b1a4422ca2b76c7fff5118641723449bcee9b0
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-05 08:50:50 +0100

    Use Elixir v1.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Jerboa.Mixfile do
     [app: :jerboa,
      version: "0.1.0",
      description: "STUN/TURN encoder, decoder and client library",
-     elixir: "~> 1.4-rc",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
