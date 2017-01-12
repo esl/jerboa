b130be0c2e93892d43a1abbb71780a9cc127e454
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-12 10:24:20 +0100

    Remove support for Mapped Address attribute
    This attribute is present in STUN RFC only for compatibility
    with older STUN clients.

--- lib/jerboa/format/body/attribute.ex
+++ lib/jerboa/format/body/attribute.ex
@@ -8,6 +8,5 @@ defmodule Jerboa.Format.Body.Attribute do
 
   defstruct [:name, :value]
 
-  def decode(0x0001, v), do: Attribute.MappedAddress.decode v
   def decode(0x0020, v), do: Attribute.XORMappedAddress.decode v
 end
