f86b5d7b826285763aa6c7dfd99b04e2575fbea9
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-16 17:24:22 +0100

    Add property tests for XORMappedAddress decoding

---
+++ test/jerboa/format/body/attribute/xor_mapped_address_test.exs
@@ -0,0 +1,123 @@
+defmodule Jerboa.Format.Body.Attribute.XORMappedAddressTest do
+  use ExUnit.Case
+  use Quixir
+
+  alias Jerboa.Format
+  alias Jerboa.Format.XORMappedAddress.{IPFamilyError, LengthError, IPArityError}
+  alias Jerboa.Format.Body.Attribute
+  alias Jerboa.Format.Body.Attribute.XORMappedAddress
+  alias Jerboa.Format.Header.MagicCookie
+
+  describe "decode/2" do
+    test "IPv4 XORMappedAddress attribute" do
+      ptest port: port_gen(), ip_addr: ip4_gen() do
+        x_ip_addr = x_ip4_addr(ip_addr)
+        x_port = x_port(port)
+        attr = <<padding()::8, ip_4()::8, x_port::16-bits, x_ip_addr::32-bits>>
+        body = <<0x0020::16, 8::16, attr::binary>>
+
+        result = XORMappedAddress.decode(%Format{body: body, length: 12}, attr)
+
+        assert {:ok, %Attribute{name: XORMappedAddress, value: val}} = result
+        assert val == %XORMappedAddress{
+                        family: :ipv4,
+                        address: ip_addr,
+                        port: port}
+      end
+    end
+
+    test "IPv6 XORMappedAddress attribute" do
+      ptest port: port_gen(), ip_addr: ip6_gen(), id: int(min: 0) do
+        identifier = <<id::96>>
+        x_ip_addr = x_ip6_addr(ip_addr, identifier)
+        x_port = x_port(port)
+        attr = <<padding()::8, ip_6()::8, x_port::16-bits, x_ip_addr::128-bits>>
+        body = <<0x0020::16, 20::16, attr::binary>>
+
+        result = XORMappedAddress.decode(%Format{identifier: identifier, body: body, length: 24}, attr)
+
+        assert {:ok, %Attribute{name: XORMappedAddress, value: val}} = result
+        assert val == %XORMappedAddress{
+                        family: :ipv6,
+                        address: ip_addr,
+                        port: port}
+      end
+    end
+
+    test "fails when attribute's value has invalid length" do
+      ptest length: int(min: 9, max: 19), content: int(min: 0) do
+        bit_length = length * 8
+        attr = <<content::size(bit_length)>>
+        body = <<0x0020::16, length::16, attr::binary>>
+
+        {:error, error} = XORMappedAddress.decode(%Format{body: body, length: byte_size(body)}, attr)
+
+        assert %LengthError{length: ^length} = error
+      end
+    end
+
+    test "fails when address family is invalid" do
+      ptest family: int(min: 3, max: 255), content: int(min: 0) do
+        length = Enum.random([20, 8])
+        # the length of attribute value minu first zeroed byte and family
+        content_length = length * 8 - 16
+        attr = <<padding()::8, family::8, content::size(content_length)>>
+        body = <<0x0020::16, length::16, attr::binary>>
+
+        {:error, error} = XORMappedAddress.decode(%Format{body: body, length: byte_size(body)}, attr)
+
+        assert %IPFamilyError{number: ^family} = error
+      end
+    end
+
+    test "fails when address does not match family" do
+      for {family, addr_len} <- [{0x01, 128}, {0x02, 32}] do
+        attr = <<padding()::8, family::8, 0::16, 0::size(addr_len)>>
+        body = <<0x0020::16, byte_size(attr)::16, attr::binary>>
+
+        {:error, error} = XORMappedAddress.decode(%Format{body: body, length: byte_size(body)}, attr)
+        assert %IPArityError{family: <<^family::8>>} = error
+      end
+    end
+  end
+
+  defp padding, do: 0
+
+  defp ip_4, do: 0x01
+
+  defp ip_6, do: 0x02
+
+  defp x_port(port) do
+    :crypto.exor(<<port::16>>, most_significant_magic_16())
+  end
+
+  defp x_ip4_addr({a3, a2, a1, a0}) do
+    :crypto.exor(<<a3::8, a2::8, a1::8, a0::8>>, MagicCookie.encode())
+  end
+
+  defp x_ip6_addr(ip6_addr, identifier) do
+    bin_addr = ip6_addr |> Tuple.to_list() |> :erlang.list_to_binary()
+    :crypto.exor(bin_addr, MagicCookie.encode() <> identifier)
+  end
+
+  defp most_significant_magic_16 do
+    <<x::16-bits, _::16>> = MagicCookie.encode()
+    x
+  end
+
+  defp port_gen do
+    int(min: 0, max: 65_535)
+  end
+
+  defp ip4_gen do
+    tuple(like: :erlang.make_tuple(4, byte()))
+  end
+
+  defp ip6_gen do
+    tuple(like: :erlang.make_tuple(16, byte()))
+  end
+
+  defp byte do
+    int(min: 0, max: 255)
+  end
+end
--- test/jerboa/format/body_test.exs
+++ test/jerboa/format/body_test.exs
@@ -1,37 +1,7 @@
 defmodule Jerboa.Format.BodyTest do
   use ExUnit.Case, async: true
   alias Jerboa.Format
-  alias Jerboa.Format.Body
   alias Jerboa.Format.Body.Attribute
-
-  @identifier :crypto.strong_rand_bytes(div(96, 8))
-
-  describe "Body.decode/1" do
-
-    test "IPv4 XORMappedAddress attribute" do
-      b = <<0x0020::16, 8::16, padding()::8, ip_4()::8, port()::16-bits, ip_4_addr()::32-bits>>
-
-      assert {:ok, %Jerboa.Format{attributes: [x]}} = Body.decode(%Jerboa.Format{body: b, length: 12})
-      assert x == %Attribute{
-        name: Attribute.XORMappedAddress,
-        value: %Attribute.XORMappedAddress{
-          family: :ipv4,
-          address: {0,0,0,0},
-          port: 0}}
-    end
-
-    test "IPv6 XORMappedAddress attribute" do
-      b = <<0x0020::16, 20::16, padding()::8, ip_6()::8, port()::16-bits, ip_6_addr()::128-bits>>
-
-      assert {:ok, %Jerboa.Format{attributes: [x]}} = Body.decode(%Jerboa.Format{identifier: @identifier, body: b, length: 24})
-      assert x == %Attribute{
-        name: Attribute.XORMappedAddress,
-        value: %Attribute.XORMappedAddress{
-          family: :ipv6,
-          address: {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-          port: 0}}
-    end
-  end
 
   describe "Body.Attribute.decode/1" do
 
@@ -40,31 +10,6 @@ defmodule Jerboa.Format.BodyTest do
         assert {:error, %Format.ComprehensionError{attribute: x}} == Attribute.decode(%Jerboa.Format{}, x, <<>>)
       end
     end
-  end
-
-  defp padding, do: 0
-
-  defp ip_4, do: 0x01
-
-  defp ip_6, do: 0x02
-
-  defp port do
-    :crypto.exor(<<0::16>>, most_significant_magic_16())
-  end
-
-  defp ip_4_addr do
-    :crypto.exor(<<0::32>>, magic_cookie())
-  end
-
-  defp ip_6_addr do
-    :crypto.exor(<<0::128>>, magic_cookie() <> @identifier)
-  end
-
-  defp magic_cookie, do: <<0x2112A442 :: 32>>
-
-  defp most_significant_magic_16 do
-    <<x::16-bits, _::16>> = magic_cookie()
-    x
   end
 
   defp known do
