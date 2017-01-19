16addac479c5a5b4d6404f617be570d5319d32bd
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-19 10:03:55 +0100

    Fix dialyzer warnings about exception/1 callbacks

--- lib/jerboa/format/exceptions.ex
+++ lib/jerboa/format/exceptions.ex
@@ -1,7 +1,8 @@
 defmodule Jerboa.Format.HeaderLengthError do
   defexception [:message, :binary]
 
-  def exception(binary: b) do
+  def exception(opts) do
+    b = opts[:binary]
     %__MODULE__{binary: b,
                 message: "the STUN wire format requires a header of at least" <>
                   " 20 bytes but got #{byte_size b} bytes"}
@@ -11,8 +12,8 @@ end
 defmodule Jerboa.Format.BodyLengthError do
   defexception [:message, :length]
 
-  def exception(length: l) do
-    %__MODULE__{length: l,
+  def exception(opts) do
+    %__MODULE__{length: opts[:length],
                 message: "message body is shorter than specified length"}
   end
 end
@@ -20,8 +21,8 @@ end
 defmodule Jerboa.Format.First2BitsError do
   defexception [:message, :bits]
 
-  def exception(bits: b) do
-    %__MODULE__{bits: b,
+  def exception(opts) do
+    %__MODULE__{bits: opts[:bits],
                 message: "the most significant two bits of a STUN " <>
                   "message must be zeros"}
   end
@@ -30,8 +31,8 @@ end
 defmodule Jerboa.Format.MagicCookieError do
   defexception [:message, :header]
 
-  def exception(header: h) do
-    %__MODULE__{header: h,
+  def exception(opts) do
+    %__MODULE__{header: opts[:header],
                 message: "STUN message doesn't have magic cookie"}
   end
 end
@@ -39,7 +40,8 @@ end
 defmodule Jerboa.Format.UnknownMethodError do
   defexception [:message, :method]
 
-  def exception(method: m) do
+  def exception(opts) do
+    m = opts[:method]
     %__MODULE__{method: m,
                 message: "unknown STUN method 0x#{Integer.to_string(m, 16)}"}
   end
@@ -48,8 +50,8 @@ end
 defmodule Jerboa.Format.Last2BitsError do
   defexception [:message, :length]
 
-  def exception(length: l) do
-    %__MODULE__{length: l,
+  def exception(opts) do
+    %__MODULE__{length: opts[:length],
                 message: "all STUN attributes are padded to a multiple of 4 bytes" <>
                   " so the last 2 bits of this field should be zero"}
   end
@@ -58,9 +60,10 @@ end
 defmodule Jerboa.Format.ComprehensionError do
   defexception [:message, :attribute]
 
-  def exception(attribute: n) do
-    %__MODULE__{attribute: n,
-                message: "can not encode/decode comprehension required attribute #{n}"}
+  def exception(opts) do
+    a = opts[:attribute]
+    %__MODULE__{attribute: a,
+                message: "can not encode/decode comprehension required attribute #{a}"}
   end
 end
 
@@ -70,7 +73,8 @@ defmodule Jerboa.Format.XORMappedAddress do
   defmodule IPFamilyError do
     defexception [:message, :number]
 
-    def exception(number: n) do
+    def exception(opts) do
+      n = opts[:number]
       %__MODULE__{number: n,
                   message: "IP family should be for 0x01 IPv4 or 0x02 for IPv6" <>
                     " but got 0x#{Integer.to_string(n, 16)}"}
@@ -80,8 +84,8 @@ defmodule Jerboa.Format.XORMappedAddress do
   defmodule LengthError do
     defexception [:message, :length]
 
-    def exception(length: l) do
-      %__MODULE__{length: l,
+    def exception(opts) do
+      %__MODULE__{length: opts[:length],
                   message: "Invalid value length. XOR Mapped Address attribute value" <>
                     "must be 8 bytes or 20 bytes long for IPv4 and IPv6 respectively"}
     end
@@ -90,13 +94,16 @@ defmodule Jerboa.Format.XORMappedAddress do
   defmodule IPArityError do
     defexception [:message, :family]
 
-    def exception(family: <<0x01::8>>) do
-      %__MODULE__{family: <<0x01::8>>,
-                  message: "IPv4 addresses are 4 bytes long but got 16 bytes"}
-    end
-    def exception(family: <<0x02::8>>) do
-      %__MODULE__{family: <<0x02::8>>,
-                  message: "IPv6 addresses are 16 bytes long but got 4 bytes"}
+    def exception(opts) do
+      message =
+        case opts[:family] do
+          <<0x01::8>> ->
+            "IPv4 addresses are 4 bytes long but got 16 bytes"
+          <<0x02::8>> ->
+            "IPv6 addresses are 16 bytes long but got 4 bytes"
+        end
+      %__MODULE__{family: opts[:family],
+                  message: message}
     end
   end
 end
