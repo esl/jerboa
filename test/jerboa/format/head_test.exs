c1533f816d5a4995256b5fb0118066e94930220c
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2017-01-25 12:33:44 +0100

    Move Format tests which're in fact Header tests into their test module

--- test/jerboa/format/head_test.exs
+++ test/jerboa/format/head_test.exs
@@ -2,11 +2,16 @@ defmodule Jerboa.Format.HeaderTest do
   use ExUnit.Case, async: true
 
   alias Jerboa.Format.Header
+  alias Jerboa.Format.{First2BitsError, MagicCookieError, UnknownMethodError, Last2BitsError}
+  alias Jerboa.Format.Header.{MagicCookie, Type.Method, Type.Class}
+  alias Jerboa.Format.Header
   alias Jerboa.Params
+  use Quixir
 
   @i :crypto.strong_rand_bytes(div 96, 8)
   @struct %Params{class: :request, method: :binding, identifier: @i, body: <<>>}
   @binary Map.fetch!(Header.encode(@struct), :header)
+  @magic MagicCookie.value
 
   describe "Header.encode/1" do
 
@@ -33,6 +38,75 @@ defmodule Jerboa.Format.HeaderTest do
     test "correct identifier in correct place" do
       i = @i
       assert <<_::2, _::14, _::16, _::32, ^i::96-bits>> = @binary
+    end
+  end
+
+  describe "Header.decode/1" do
+
+    test "fails given packet not starting with two zeroed bits" do
+      ptest first_two: int(min: 1, max: 3), content: int(min: 0),
+            length: int(min: 20) do
+        bit_length = length * 8 - 2
+        packet = <<first_two::2, content::size(bit_length)>>
+
+        {:error, error} = Jerboa.Format.decode packet
+
+        assert %First2BitsError{bits: ^first_two} = error
+      end
+    end
+
+    test "fails given packet with invalid STUN magic cookie" do
+      ptest before_magic: int(min: 0), magic: int(min: 0, max: @magic - 1),
+            after_magic: int(min: 0), length: int(min: 12) do
+        packet = <<0::2, before_magic::30, magic::32,
+                   after_magic::unit(8)-size(length)>>
+        <<header::20-bytes, _::binary>> = packet
+
+        {:error, error} = Jerboa.Format.decode packet
+
+        assert %MagicCookieError{header: ^header} = error
+      end
+    end
+
+    test "fails if length isn't a multiple of 4" do
+      ptest length: int(min: 0), content: int(min: 0) do
+        length = if rem(length, 4) == 0, do: length + 1, else: length
+        packet = <<0::2, 1::14, length::16, @magic::32, 0::96,
+                  content::size(length)-unit(8)>>
+
+        {:error, error} = Jerboa.Format.decode packet
+
+        assert %Last2BitsError{length: ^length} = error
+      end
+    end
+
+    test "fails given packet with invalid STUN method" do
+      ptest method: int(min: 1024, max: 4095), class: int(min: 0, max: 3) do
+        <<c1::1, c0::1>> = <<class::2>>
+        <<m2::5, m1::3, m0::4>> = <<method::12>>
+        packet = <<0::2, m2::5, c1::1, m1::3, c0::1, m0::4, 0::16,
+                   @magic::32, 0::96>>
+
+        {:error, error} = Jerboa.Format.decode packet
+
+        assert %UnknownMethodError{method: ^method} = error
+      end
+    end
+
+    test "decodes class and method from the header" do
+      ptest method: int(min: 1, max: 1), class: int(min: 0, max: 3) do
+        bit_class = <<c1::1, c0::1>> = <<class::2>>
+        bit_method = <<m2::5, m1::3, m0::4>> = <<method::12>>
+        packet = <<0::2, m2::5, c1::1, m1::3, c0::1, m0::4, 0::16,
+          @magic::32, 0::96>>
+        decoded_class = Class.decode(bit_class)
+        {:ok, decoded_method} = Method.decode(bit_method)
+
+        {:ok, message} = Jerboa.Format.decode packet
+
+        assert decoded_method == message.method
+        assert decoded_class == message.class
+      end
     end
   end
 
--- test/jerboa/format_test.exs
+++ test/jerboa/format_test.exs
@@ -4,20 +4,12 @@ defmodule Jerboa.FormatTest do
 
   alias Jerboa.Test.Helper.XORMappedAddress, as: XORMAHelper
 
-  alias Jerboa.Format
-  alias Format.{HeaderLengthError,BodyLengthError}
-  alias Format.{First2BitsError,MagicCookieError,UnknownMethodError,Last2BitsError}
-  alias Format.Header.{MagicCookie,
-                     Type.Method, Type.Class}
-  alias Format.Body
-  alias Body.Attribute
-  alias Jerboa.Params
-
-  import Bitwise
+  alias Jerboa.{Format, Params}
+  alias Format.{HeaderLengthError, BodyLengthError}
+  alias Jerboa.Format.Header.MagicCookie
 
   @i :crypto.strong_rand_bytes(div 96, 8)
   @magic MagicCookie.value
-  @most_significant_magic_16 <<(@magic >>> 16) :: 16>>
 
   describe "Format.encode/1" do
 
@@ -42,72 +34,6 @@ defmodule Jerboa.FormatTest do
         packet = <<content::size(byte_length)>>
 
         assert {:error, %HeaderLengthError{binary: ^packet}} = Format.decode packet
-      end
-    end
-
-    test "fails given packet not starting with two zeroed bits" do
-      ptest first_two: int(min: 1, max: 3), content: int(min: 0),
-            length: int(min: 20) do
-        bit_length = length * 8 - 2
-        packet = <<first_two::2, content::size(bit_length)>>
-
-        {:error, error} = Format.decode packet
-
-        assert %First2BitsError{bits: ^first_two} = error
-      end
-    end
-
-    test "fails given packet with invalid STUN magic cookie" do
-      ptest before_magic: int(min: 0), magic: int(min: 0, max: @magic - 1),
-            after_magic: int(min: 0), length: int(min: 12) do
-        packet = <<0::2, before_magic::30, magic::32,
-                   after_magic::unit(8)-size(length)>>
-        <<header::20-bytes, _::binary>> = packet
-
-        {:error, error} = Format.decode packet
-
-        assert %MagicCookieError{header: ^header} = error
-      end
-    end
-
-    test "fails if length isn't a multiple of 4" do
-      ptest length: int(min: 0), content: int(min: 0) do
-        length = if rem(length, 4) == 0, do: length + 1, else: length
-        packet = <<0::2, 1::14, length::16, @magic::32, 0::96,
-                  content::size(length)-unit(8)>>
-
-        {:error, error} = Format.decode packet
-
-        assert %Last2BitsError{length: ^length} = error
-      end
-    end
-
-    test "fails given packet with invalid STUN method" do
-      ptest method: int(min: 1024, max: 4095), class: int(min: 0, max: 3) do
-        <<c1::1, c0::1>> = <<class::2>>
-        <<m2::5, m1::3, m0::4>> = <<method::12>>
-        packet = <<0::2, m2::5, c1::1, m1::3, c0::1, m0::4, 0::16,
-                   @magic::32, 0::96>>
-
-        {:error, error} = Format.decode packet
-
-        assert %UnknownMethodError{method: ^method} = error
-      end
-    end
-
-    test "decodes class and method from the header" do
-      ptest method: int(min: 1, max: 1), class: int(min: 0, max: 3) do
-        bit_class = <<c1::1, c0::1>> = <<class::2>>
-        bit_method = <<m2::5, m1::3, m0::4>> = <<method::12>>
-        packet = <<0::2, m2::5, c1::1, m1::3, c0::1, m0::4, 0::16,
-          @magic::32, 0::96>>
-        decoded_class = Class.decode(bit_class)
-        {:ok, decoded_method} = Method.decode(bit_method)
-
-        {:ok, message} = Format.decode packet
-
-        assert decoded_method == message.method
-        assert decoded_class == message.class
       end
     end
 
@@ -147,7 +73,7 @@ defmodule Jerboa.FormatTest do
     test "raises an exception upon failure" do
       packet = "Supercalifragilisticexpialidocious!"
 
-      assert_raise First2BitsError, fn -> Format.decode!(packet) end
+      assert_raise Jerboa.Format.First2BitsError, fn -> Format.decode!(packet) end
     end
 
     test "returns value without an :ok tuple" do
