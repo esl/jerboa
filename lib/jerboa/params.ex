0e036bca20bca2822b32be39bd5bc79b3798e91d
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-01-27 11:26:51 +0100

    Add setters and getters for Params struct

--- lib/jerboa/params.ex
+++ lib/jerboa/params.ex
@@ -43,4 +43,132 @@ defmodule Jerboa.Params do
     body: binary,
     extra: binary
   }
+
+  @doc """
+  Returns params struct with filled in transaction id
+  """
+  @spec new :: t
+  def new do
+    %__MODULE__{identifier: generate_id()}
+  end
+
+  @doc """
+  Sets STUN class in params struct
+  """
+  @spec put_class(t, Class.t) :: t
+  def put_class(params, class) do
+    %{params | class: class}
+  end
+
+  @doc """
+  Retrieves class field from params struct
+  """
+  @spec get_class(t) :: Class.t | nil
+  def get_class(%__MODULE__{class: class}), do: class
+
+  @doc """
+  Sets STUN method in params struct
+  """
+  @spec put_method(t, Method.t) :: t
+  def put_method(params, method) do
+    %{params | method: method}
+  end
+
+  @doc """
+  Retrieves method field from params struct
+  """
+  @spec get_method(t) :: Method.t | nil
+  def get_method(%__MODULE__{method: method}), do: method
+
+  @doc """
+  Sets STUN transaction identifier in params struct
+  """
+  @spec put_id(t, binary) :: t
+  def put_id(params, id) do
+    %{params | identifier: id}
+  end
+
+  @doc """
+  Retrieves transaction ID from params struct
+  """
+  @spec get_id(t) :: binary | nil
+  def get_id(%__MODULE__{identifier: id}), do: id
+
+  @doc """
+  Retrieves length field from params struct
+  """
+  @spec get_length(t) :: non_neg_integer | nil
+  def get_length(%__MODULE__{length: l}), do: l
+
+  @doc """
+  Retrieves body field from params struct
+  """
+  @spec get_body(t) :: binary | nil
+  def get_body(%__MODULE__{body: b}), do: b
+
+  @doc """
+  Retrieves header field from params struct
+  """
+  @spec get_header(t) :: binary | nil
+  def get_header(%__MODULE__{header: h}), do: h
+
+  @doc """
+  Retrieves excess binary field from params struct
+  """
+  @spec get_extra(t) :: binary | nil
+  def get_extra(%__MODULE__{extra: e}), do: e
+
+  @doc """
+  Retrieves all attributes from params struct
+  """
+  @spec get_attrs(t) :: [Attribute.t]
+  def get_attrs(%__MODULE__{attributes: attrs}), do: attrs
+
+  @doc """
+  Sets whole attributes list in params struct
+  """
+  @spec put_attrs(t, [Attribute.t]) :: t
+  def put_attrs(params, attrs) do
+    %{params | attributes: attrs}
+  end
+
+  @doc """
+  Retrieves single attribute from params struct
+
+  Returns `nil` if attribute is not present.
+  """
+  @spec get_attr(t, attr_name :: module) :: Attribute.t
+  def get_attr(params, attr_name) do
+    params.attributes
+    |> Enum.find(fn a -> a.name === attr_name end)
+  end
+
+  @doc """
+  Puts single attribute in params struct
+
+  If given attribute was already present, it will be overriden.
+
+  When this function is given plain attribute struct, not wrapped
+  in `Jerboa.Body.Attribute`, the name of attribute will be inferred
+  from struct name.
+  """
+  @spec put_attr(t, Attribute.t | struct) :: t
+  def put_attr(params, %Attribute{name: name} = attr) do
+    attrs =
+      params.attributes
+      |> Enum.reject(fn a -> a.name === name end)
+    %{params | attributes: [attr | attrs]}
+  end
+  def put_attr(params, %{__struct__: name} = attr) do
+    put_attr(params, %Attribute{name: name, value: attr})
+  end
+
+
+  @doc """
+  Generates STUN transaction ID
+  """
+  @spec generate_id :: binary
+  def generate_id do
+    :crypto.strong_rand_bytes(12)
+  end
 end
---
+++ test/jerboa/params_test.exs
@@ -0,0 +1,153 @@
+defmodule Jerboa.ParamsTest do
+  use ExUnit.Case
+
+  alias Jerboa.Params
+  alias Jerboa.Format.Body.Attribute
+  alias Attribute.XORMappedAddress
+
+  test "new/0 returns fresh params struct with ID set" do
+    p = Params.new
+
+    assert p.identifier != nil
+    assert byte_size(p.identifier) == 12
+  end
+
+  test "put_class/2 sets class field" do
+    p = Params.new
+    class = :request
+
+    p = Params.put_class(p, class)
+
+    assert p.class == class
+  end
+
+  test "get_class/1 retrieves class field" do
+    class = :request
+    p = Params.new() |> Params.put_class(class)
+
+    assert class == Params.get_class(p)
+  end
+
+  test "put_method/2 sets method field" do
+    p = Params.new
+    method = :binding
+
+    p = Params.put_method(p, method)
+
+    assert p.method == method
+  end
+
+  test "get_method/1 retrieves method field" do
+    method = :binding
+    p = Params.new() |> Params.put_method(method)
+
+    assert method == Params.get_method(p)
+  end
+
+  test "put_id/2 sets identifier field" do
+    p = Params.new
+    id = Params.generate_id()
+
+    p = Params.put_id(p, id)
+
+    assert p.identifier == id
+  end
+
+  test "get_id/1 retrieves identifier field" do
+    id = Params.generate_id()
+    p = Params.new() |> Params.put_id(id)
+
+    assert id == Params.get_id(p)
+  end
+
+  test "get_body/1 retrieves body field" do
+    body = "hello"
+    p = Params.new() |> Map.put(:body, body)
+
+    assert body == Params.get_body(p)
+  end
+
+  test "get_header/1 retrieves header field" do
+    header = "hello"
+    p = Params.new() |> Map.put(:header, header)
+
+    assert header == Params.get_header(p)
+  end
+
+  test "get_extra/1 retrieves extra field" do
+    extra = "hello"
+    p = Params.new() |> Map.put(:extra, extra)
+
+    assert extra == Params.get_extra(p)
+  end
+
+  test "get_length/1 retrieves length field" do
+    length = 120
+    p = Params.new() |> Map.put(:length, length)
+
+    assert length == Params.get_length(p)
+  end
+
+  test "put_attrs/1 sets whole attributes list" do
+    attrs = List.duplicate(%Attribute{}, 3)
+
+    p = Params.new() |> Params.put_attrs(attrs)
+
+    assert p.attributes == attrs
+  end
+
+  test "get_attrs/1 retrieves whole attributes list" do
+    attrs = List.duplicate(%Attribute{}, 3)
+    p = Params.new |> Params.put_attrs(attrs)
+
+    assert attrs == Params.get_attrs(p)
+  end
+
+  describe "put_attr/2" do
+    test "adds attribute to attributes list in params struct" do
+      attr = %Attribute{name: XORMappedAddress,
+                        value: %XORMappedAddress{family: :ipv4,
+                                                 address: {127, 0, 0, 1},
+                                                 port: 3333}}
+
+      p = Params.new() |> Params.put_attr(attr)
+
+      assert [attr] == Params.get_attrs(p)
+    end
+
+    test "infers attributes name when adding to attributes list" do
+      attr = %XORMappedAddress{family: :ipv4, address: {127, 0, 0, 1}, port: 3333}
+
+      p = Params.new() |> Params.put_attr(attr)
+
+      assert [%Attribute{name: XORMappedAddress, value: attr}] == Params.get_attrs(p)
+    end
+
+    test "overrides existing attribute with the same name" do
+      attr1 = %XORMappedAddress{family: :ipv4, address: {127, 0, 0, 1}, port: 3333}
+      attr2 = %XORMappedAddress{family: :ipv, address: {0, 0, 0, 0, 0, 0, 0, 1},
+                                port: 3333}
+
+      p = Params.new() |> Params.put_attr(attr1) |> Params.put_attr(attr2)
+
+      assert [%Attribute{name: XORMappedAddress, value: attr2}] == Params.get_attrs(p)
+    end
+  end
+
+  describe "get_attr/2" do
+    test "retrieves attribute by its name" do
+      attr = %XORMappedAddress{family: :ipv4, address: {127, 0, 0, 1}, port: 3333}
+
+      p = Params.new() |> Params.put_attr(attr)
+
+      expected = %Attribute{value: attr, name: XORMappedAddress}
+      assert expected == Params.get_attr(p, XORMappedAddress)
+    end
+
+    test "returns nil if attribute is not present" do
+      p = Params.new
+
+      assert nil == Params.get_attr(p, XORMappedAddress)
+    end
+  end
+end
